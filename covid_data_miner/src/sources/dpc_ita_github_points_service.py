0080f23c51f0dadc1039808ba69d0b3b232ef593
gdassori <guido.dassori@gmail.com>
2020-03-31 19:01:20 +0200

    use json dpc_ita source instead of csv

--- covid_data_miner/src/sources/dpc_ita_github_points_service.py
+++ covid_data_miner/src/sources/dpc_ita_github_points_service.py
@@ -1,8 +1,7 @@
-import csv
 import datetime
 import typing
-from github import Github
-import base64
+
+import requests
 
 from covid_data_miner.src.domain import CovidPoint
 
@@ -15,9 +14,7 @@ class DPCItaGithubPointsService:
     tags = ['region']
 
     def __init__(self, authentication_key):
-        self.repo_name = 'pcm-dpc/COVID-19'
-        self.folder = '/dati-regioni/'
-        self.repo = Github(login_or_token=authentication_key or None, per_page=1000)
+        pass
 
     @staticmethod
     def _filename_to_datetime(filename):
@@ -25,26 +22,11 @@ class DPCItaGithubPointsService:
         d = datetime.datetime.strptime(filedate, '%Y%m%d%H%M%S')
         return d
 
-    def _fetch_data(self):
-        repo = self.repo.get_repo(self.repo_name)
-        content = repo.get_contents(self.folder + 'dpc-covid19-ita-regioni.csv')
-        return base64.b64decode(content.content).decode()
-
-    @staticmethod
-    def _parse_csv(entry: str):
+    def _get_points(self, data: typing.List[typing.Dict], min_timestamp=None) -> typing.List[CovidPoint]:
         res = []
-        for row in csv.reader([x for x in entry.split('\n') if x], delimiter=','):
-            res.append(row)
-        return res
-
-    def _get_points(self, data: str, min_timestamp=None) -> typing.List[CovidPoint]:
-        res = []
-        rows = self._parse_csv(data)
-        for row in rows:
-            if 'data' == row[0]:
-                continue
-            dt_format = '%Y-%m-%dT%H:%M:%S' if 'T' in row[0] else '%Y-%m-%d %H:%M:%S'
-            updated_at = datetime.datetime.strptime(row[0], dt_format)
+        for row in data:
+            dt_format = '%Y-%m-%dT%H:%M:%S' if 'T' in row['data'] else '%Y-%m-%d %H:%M:%S'
+            updated_at = datetime.datetime.strptime(row['data'], dt_format)
             if min_timestamp and updated_at < datetime.datetime.fromtimestamp(min_timestamp):
                 continue
             point = CovidPoint(
@@ -52,29 +34,32 @@ class DPCItaGithubPointsService:
                 timestamp=updated_at,
                 last_update=int(updated_at.strftime('%s')),
                 country="Italy",
-                region=row[3].replace("'", "."),
+                region=row['denominazione_regione'].replace("'", "."),
                 city="",
-                confirmed_cumulative=int(row[14]),
-                death_cumulative=int(row[13]),
-                recovered_cumulative=int(row[12]),
-                hospitalized_cumulative=int(row[8]),
-                severe_cumulative=int(row[7]),
-                tests_cumulative=int(row[15])
+                confirmed_cumulative=int(row['totale_positivi']),
+                death_cumulative=int(row['deceduti']),
+                recovered_cumulative=int(row['dimessi_guariti']),
+                hospitalized_cumulative=int(row['totale_ospedalizzati']),
+                severe_cumulative=int(row['terapia_intensiva']),
+                tests_cumulative=int(row['tamponi'])
             )
             res.append(point)
         return res
 
     def get_points_since(self, timestamp: int):
-        data = self._fetch_data()
+        data = requests.get(
+            'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json'
+        ).json()
         points = self._get_points(data, timestamp)
         return points
 
     def get_last_update(self) -> int:
-        data = self._fetch_data()
-        _csv = self._parse_csv(data)
+        data = requests.get(
+            'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni-latest.json'
+        ).json()
         try:
-            dt_format = '%Y-%m-%dT%H:%M:%S' if 'T' in _csv[-1][0] else '%Y-%m-%d %H:%M:%S'
-            dt = datetime.datetime.strptime(_csv[-1][0], dt_format)
+            dt_format = '%Y-%m-%dT%H:%M:%S' if 'T' in data[-1]['data'] else '%Y-%m-%d %H:%M:%S'
+            dt = datetime.datetime.strptime(data[-1]['data'], dt_format)
         except Exception as e:
             print(e)
             dt = None
